70ee924f2e9505f015d76af853cd76d25907ae28
sush80 <sush@Sushs-MacBook-Pro.local>
2015-11-14 21:01:33 +0100

    temp

--- MotorCtrlFast_Sim.py
+++ MotorCtrlFast_Sim.py
@@ -21,13 +21,13 @@ plotWindowLength_ms = 2000
 simulationTime_ms = 0
 timeList_ms = []
 speedList = []
-motorFastSpeed = 0
+motorFastSpeed = -100
 
 
 fig = plt.figure(1)
 
 plot221 = plt.subplot(221)
-line, = plt.plot(timeList_ms, speedList, 'bo')
+data221, = plt.plot(timeList_ms, speedList, 'bo')
 plt.title('Motorctrl_Fast')
 plt.grid(True)
 
@@ -37,7 +37,7 @@ plot221.axes.set_ylim([-110,+110])
 
 
 #line, = plot(0,0)
-for i in range(1,30):
+for i in range(1,300):
     ######################
     #Perform Calculations
     motorFastSpeed = motorFast.MotorCtrlFast_Cyclic(simulationTimeIncrement_ms, motorFastSpeed, simulationTargetSpeed)
@@ -46,18 +46,27 @@ for i in range(1,30):
     #Do the plotting stuff
     timeList_ms.append(simulationTime_ms)
     speedList.append(motorFastSpeed)
-    line.set_xdata(timeList_ms[:])
-    line.set_ydata(speedList[:])
+    data221.set_xdata(timeList_ms[:])
+    data221.set_ydata(speedList[:])
     if (simulationTime_ms < plotWindowLength_ms):
         plot221.axes.set_xlim([0,plotWindowLength_ms])
     else:
         plot221.axes.set_xlim([simulationTime_ms-plotWindowLength_ms,simulationTime_ms+100])
         
     plt.draw()
-    print "turn"
+    #print "turn"
     
     ######################
     #Housekeeping
     simulationTime_ms += 100
     time.sleep(0.1)
 
+
+    if i == 50:
+        simulationTargetSpeed = -50
+    if i == 100:
+        simulationTargetSpeed = -100
+    if i == 150:
+        simulationTargetSpeed = 0
+
+print "done"
